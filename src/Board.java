b8c015acbbf8fd0d077bb4f87a47fdfe34eb2a45
Alex Bowker <alex.bowker@salfordcc.ac.uk>
2025-03-27 14:28:05 +0000

    Add basic window

---
+++ src/Board.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel {
+
+    public Board() {
+        setPreferredSize(new Dimension(400, 400));
+        setBackground(Color.CYAN);
+    }
+
+}
