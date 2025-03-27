8517a7414aff82d37daa18fb915a291127c665ca
xishax <ishaheaven27@gmail.com>
2025-03-27 14:28:03 +0000

    Add basic window

---
+++ src/Board.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel {
+    public Board() {
+        setPreferredSize(new Dimension(1000,800));
+        setBackground(Color.pink);
+    }
+}
