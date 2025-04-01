fb8aa1f8686295590e4032665ae87fb5c3b0c613
Alex Bowker <alex.bowker@salfordcc.ac.uk>
2025-04-01 10:50:01 +0100

    Add constants file

--- src/Board.java
+++ src/Board.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 
+import static utils.Constants.BOARD_HEIGHT;
+import static utils.Constants.BOARD_WIDTH;
+
 public class Board extends JPanel {
 
     public Board() {
-        setPreferredSize(new Dimension(400, 400));
+        setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
         setBackground(Color.CYAN);
     }
 
---
+++ src/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+public final class Constants {
+    private Constants() {
+        // prevents instantiation
+    }
+
+    public static final int BOARD_WIDTH = 640;
+    public static final int BOARD_HEIGHT = 480;
+}
